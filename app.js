83a10812b509e825d8a61bedb703c705b9307421
PCCP1234 <147849952+PCCP1234@users.noreply.github.com>
2024-02-07 19:28:04 +0000

    remove tasks

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ let data = [
     {'task': "netflix", "status": 'checked'}
 ];
 
-const newItem = (task, status) => {
+const newItem = (task, status, index) => {
     const item = document.createElement('label');
     item.classList.add('todo_item');
     item.innerHTML = `
-    <input type="checkbox" ${status}checked>
+    <input type="checkbox" ${status} data-index=${index}>
     <div>${task}</div>
-    <input type="button" value="x">`
+    <input type="button" value="x" data-index=${index}>`
 
     document.getElementById('todoList').appendChild(item);
 }
@@ -31,22 +31,46 @@ const cleanTasks = () => {
 
 const render = () => {
     cleanTasks();
-    data.forEach (item => newItem(item.task, item.status));
+    data.forEach ((item, index) => newItem(item.task, item.status, index));
 }
 
 const insertItem = (event) => {
     const key = event.key;
     const input = event.target.value;
     if( key === 'Enter'){
-        data.push({'task': input, 'status': ' '});
+        data.push({'task': input, 'status': ''});
         render();
+        event.target.value = '';
     }
     
     console.log(key);
 
 }
 
+const removeItem = (index) => {
+    data.splice (index, 1);
+    render();
+}
+
+const updateItem = (index) => {
+    data[index].status = data[index].status === '' ? 'checked' : '';
+    render();
+}
+
+const clickItem = (event) => {
+    const element = event.target;
+    if(element.type === 'button') {
+        const index = element.dataset.index;
+        removeItem(index);
+    } else if (element.type === 'checkbox') {
+        const index = element.dataset.index;
+        updateItem(index);
+    }
+    console.log(element);
+}
+
 document.getElementById('newItem').addEventListener('keypress', insertItem);
+document.getElementById('todoList').addEventListener('click', clickItem)
 
 render();
 
